b33999fa1a4f36b82d16c2679ea952e761df5b1d
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 14:15:04 +1100

    Add Variables
    Added instance variables: rideName, capacity, assignedEmployee

--- Ride.java
+++ Ride.java
@@ -1,2 +1,9 @@
 // Will represent ride in the park
-public class Ride {}
+public class Ride {
+    
+    // Variables to represent ride details
+    private String rideName;                            // Name of the ride in theme park
+    private int capacity;                               // Max capacity of ride
+    private Employee assignedEmployee;                  // Employee that will be assigned to operate the ride
+
+}
