280bf61fa769d68912668166855addba113f3d18
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 12:32:19 +1100

    Add variables
    Added instance variables: jobTitle, employeeID to Employee class.

--- Employee.java
+++ Employee.java
@@ -1,6 +1,9 @@
 // Employee working in the theme park, will extend Person class
 
 public class Employee {
-
+    
+    // Instance variables that will be specific to employee
+    private String jobTitle;
+    private String employeeID;
 
 }
