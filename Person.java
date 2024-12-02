264d0786186ae5858a759deef218556caca4b5e7
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 12:26:05 +1100

    Add getter & setter
    Added getters and setters for Person class variables, name, age & email.

--- Person.java
+++ Person.java
@@ -15,5 +15,13 @@ public class Person {
         this.email = email;
     }
 
+    // Getter and Setter for name, age & email
+    public String getName() { return name; }
+    public void setName (String name) { this.name = name; }
 
+    public int getAge() { return age; }
+    public void setAge (int age) { this.age = age; }
+
+    public String getEmail() { return email; }
+    public void setEmail (String email) { this.email = email; }
 }
