7aceba68470bd138d283450cba1d711a695d3f2e
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 13:02:30 +1100

    Add vairables
    Add instance variable ticketID & visitDate

--- Visitor.java
+++ Visitor.java
@@ -1,6 +1,9 @@
 import java.util.Date;              // Import for date handling
 
 // Visitor of the theme park, extending the Person class
-public class Visitor {
+public class Visitor extends Person { 
     
+    // Instance variables for visitor (specific)
+    private String ticketID;            // Ticket ID of visitor
+    private Date visiDate;              // Visit date of the visitor
 }
