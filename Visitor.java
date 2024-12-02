e5894ccf92a04e56bacb7497ca7b280701aae62d
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 13:12:52 +1100

    Add Constructors
    Added default and parameterized constructors to Visitor class.

--- Visitor.java
+++ Visitor.java
@@ -6,4 +6,14 @@ public class Visitor extends Person {
     // Instance variables for visitor (specific)
     private String ticketID;            // Ticket ID of visitor
     private Date visitDate;              // Visit date of the visitor
+
+    // Default constructor
+    public Visitor() {}
+
+    // Parameterised constructor for both Person & Visitor details
+    public Visitor (String name, int age, String email, String ticketID, Date visitDate) {
+        super (name, age, email);           // Call to Person constructor
+        this.ticketID = ticketID;
+        this.visitDate = visitDate;
+    }
 }
