5bee05f9f52e7573fb530c3152cf8a02b5d5c678
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 11:50:30 +1100

    Add Constructors
    Added default and parameterized constructors to Person class

--- Person.java
+++ Person.java
@@ -1,5 +1,19 @@
+// Person with basic attributes
+
 public class Person {
     private String name;
     private int age;
     private String email;
+
+    // Default constructor
+    public Person() {}
+
+    // Parameterised constructor to start a person's details
+    public Person (String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+
 }
