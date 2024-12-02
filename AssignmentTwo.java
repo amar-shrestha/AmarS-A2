7a6ce0cf81bca4a5ec587ba26cbcbdd52b6c81d7
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-02 16:30:20 +1100

    Added placeholder methods for Parts 3 to 7 in AssignmentTwo class

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -4,4 +4,18 @@ public class AssignmentTwo {
     public static void main (String[] argas) {
         System.out.println ("Welcome to the Theme Park Management System!");
     }
+
+    // Placeholder method for now
+    public void partThree () {}                  // part 3 will go here
+    
+    public void partFourA () {}                 // part FourA will go here
+
+    public void partFourB () {}                 // part FourB will go here
+
+    public void partFive () {}                  // part Five will go here
+
+    public void partSix () {}                   // part Six will go here
+
+    public void partSeven () {}                 // part Seven will go here
+
 }
