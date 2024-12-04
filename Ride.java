b745385e81fe2e9b9f87120bae7142280e5d51fd
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-04 21:43:35 +1100

    incorrect code position, redact

--- Ride.java
+++ Ride.java
@@ -28,9 +28,4 @@ public class Ride {
     public void setAssignedEmployee (Employee assignedEmployee) {               // Setter
         this.assignedEmployee = assignedEmployee; }
     
-    // Represents ride in theme park
-    public class Ride implements RideInterface {
-        
-        // Class implementation will follow
-    }
 }
