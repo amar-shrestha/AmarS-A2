55e8c764a8dc9eae8cac113bec9c2c6c1eee7efc
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-06 22:53:34 +1100

    Fixed RideInterface
    This was incorrectly defined as public class instead of interface and not a class.

--- Ride.java
+++ Ride.java
@@ -1,5 +1,7 @@
 // Will represent ride in the park
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
--- RideInterface.java
+++ RideInterface.java
@@ -1,6 +1,6 @@
 // Interface to define operations for theme park ride
 
-public class RideInterface {
+public interface RideInterface {
     
     // Add visitor to the queue
     void addVisitorToQueue (Visitor visitor);
